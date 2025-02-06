616694970e24ca53aa98220cc2ed671ac3e15017
nithiyah <Nithiyah02@gmail.com>
2025-02-06 15:20:11 +0800

    nav bar- org&vol. logo not done yet

--- nexus/settings.py
+++ nexus/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'nexus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,3 +131,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
