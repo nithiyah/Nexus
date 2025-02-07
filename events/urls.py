da99df634b4aa3d4f0e01606269ca963367f03ab
nithiyah <Nithiyah02@gmail.com>
2025-02-07 12:00:41 +0800

    events urls.py register and my events page

--- events/urls.py
+++ events/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('create/', views.create_event, name='create_event'),
     path('edit/<int:event_id>/', views.edit_event, name='edit_event'),
     path('delete/<int:event_id>/', views.delete_event, name='delete_event'),
+    path('my-events/', views.volunteer_events, name='volunteer_events'),
+    path('register/<int:event_id>/', views.register_for_event, name='register_for_event'),
+
+
 ]
